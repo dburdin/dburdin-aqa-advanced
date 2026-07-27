10673f66bc9a8bbb442190503d6745cf363d670a
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2026-07-27 21:53:57 +0300

    adding task1, hw-5.1

---
+++ jsLoops/task1.js
@@ -0,0 +1,17 @@
+const averageGrade = 710;
+
+if (averageGrade < 60) {
+  console.log('Незадовільно');
+} else if (averageGrade <= 70) {
+  console.log('Задовільно');
+} else if (averageGrade <= 80) {
+  console.log('Добре');
+} else if (averageGrade <= 90) {
+  console.log('Дуже добре');
+} else if (averageGrade <= 100) {
+  console.log('Відмінно');
+} else {
+  console.log(
+    'Введіть значення із діапазону від 0 до 100. Поточна середня оцінка: ' + averageGrade
+  );
+}
