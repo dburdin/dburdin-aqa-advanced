0aa0288dfe44dc0051638869affde3b89955f009
Dmytrii Bevziuk <dbevziuk.tech@gmail.com>
2026-07-27 19:59:29 +0300

    Feat git js basics (#3)
    * adding task1 and task2
    
    * adding task3, task4
    
    ---------
    
    Co-authored-by: dbvzk <dmytrii.bevziuk@difreight.com>

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const firstName = 'Alex';
+const secondName = 'Mango';
+
+const concatGreetings = 'Hello' + ' ' + firstName + ' ' + 'and' + ' ' + secondName + '!';
+const templateGreetings = `Hello ${firstName} and ${secondName}!`;
+
+console.log('🚀 ~ concatGreetings:', concatGreetings);
+console.log('🚀 ~ templateGreetings:', templateGreetings);
---
+++ jsBasics/task4.js
@@ -0,0 +1,23 @@
+// Завдання 4.1
+
+const radius = 11;
+const A = radius ** 2 * Math.PI;
+
+console.log('Площа кола:', A);
+
+// Завдання 4.2
+
+const length = 55;
+const width = 10;
+const S = length * width;
+
+console.log('Площа прямокутника:', S);
+
+// Завдання 4.3
+
+const radius2 = 2;
+const height = 5;
+const V = radius2 ** 2 * height * Math.PI;
+
+console.log('Обʼєм циліндра', V);
+console.log('Обʼєм циліндра, округлений', V.toFixed(2));
