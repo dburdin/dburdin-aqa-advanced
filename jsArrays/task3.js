42eb5aa6d9fc7a5bc26d216c7bf55dc7f8bf6a08
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2026-08-06 21:36:36 +0300

    tasks_1-6, hw-8

---
+++ jsArrays/task3.js
@@ -0,0 +1,3 @@
+const array = [10, 20, 30, 40, 50];
+const sum = array.reduce((acc, item) => acc + item, 0);
+console.log('🚀 ~ sum:', sum);
---
+++ jsArrays/task6.js
@@ -0,0 +1,3 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const sortedArray = [...numbersList].sort((a, b) => a - b);
+console.log('🚀 ~ sortedArray:', sortedArray);
